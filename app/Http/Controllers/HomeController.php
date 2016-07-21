d19ac6ac16f606026c6bd14334c20a3a0c499519
Adrian Gonzalez <adrigonle@gmail.com>
2016-07-22 01:09:57 +0200

    Updated examples with new domains

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,13 +25,13 @@ class HomeController
     public function login(Request $request, Response $response, $args)
     {
         $client_id = 'testclient';
-        $redirect_uri = 'http://192.168.33.10/login/callback';
+        $redirect_uri = 'http://willyfog.com/login/callback';
         $response_type = 'code';
         $state = 'xyz';
         $scope = 'openid';
         
         return $response->withRedirect(
-            "http://192.168.33.10:8000/authorize?client_id=$client_id&redirect_uri=$redirect_uri&response_type=$response_type&scope=$scope&state=$state"
+            "http://openid.willyfog.com/authorize?client_id=$client_id&redirect_uri=$redirect_uri&response_type=$response_type&scope=$scope&state=$state"
         );
     }
 
@@ -39,19 +39,19 @@ class HomeController
     {
         $code = $request->getQueryParam('code');
 
-        $res = (new Client())->request('POST', 'http://192.168.33.10:8000/token', [
+        $res = (new Client())->request('POST', 'http://openid.willyfog.com/token', [
             'form_params' => [
                 'grant_type'    => 'authorization_code',
                 'client_id'     => 'testclient',
                 'code'          => $code,
-                'redirect_uri'  => 'http://192.168.33.10/login/callback'
+                'redirect_uri'  => 'http://willyfog.com/login/callback'
             ]
         ]);
 
         $response = $res->getBody();
 
-        $id = base64_decode(json_decode($response)->id_token);
+        $access_token = json_decode($response)->access_token;
 
-        return "Here is your $id";
+        return "Here is your access token: $access_token";
     }
 }
