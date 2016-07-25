7065c9f1b4691af42281bac6e63a5d8ffd7ac339
Adrian Gonzalez <adrigonle@gmail.com>
2016-07-25 20:44:20 +0200

    removed constants.php

--- app/constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-defined('APP_DOMAIN') || define('APP_DOMAIN', 'willyfog.com');
-
-defined('OPENID_URI') || define('OPENID_URI', 'http://openid.willyfog.com');
-defined('API_CLIENT') || define('API_CLIENT', 'webclient');
-defined('API_SECRET') || define('API_SECRET', 'websecret');
-defined('OPENID_REDIRECT_URI') || define('OPENID_REDIRECT_URI', 'http://willyfog.com/login/callback');
